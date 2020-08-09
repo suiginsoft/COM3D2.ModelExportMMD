99b615458682bc8f15a1624b9819d46f3b726962
suiginsoft <suigin@national.shitposting.agency>
2020-08-09 15:49:16 -0700

    Reimplemented model T-pose functionality:
    - Rewrote T-pose code in data-driven fashion
    - Transformed ModelExportWindow's public Actions into events
    - Added external assembly dependencies to repository
    - Changed target framework to .NET Framework 3.5

--- COM3D2.ModelExportMMD.Plugin/ModelExportPlugin.cs
+++ COM3D2.ModelExportMMD.Plugin/ModelExportPlugin.cs
@@ -1,5 +1,6 @@
 using COM3D2.ModelExportMMD.Gui;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -17,6 +18,110 @@ namespace COM3D2.ModelExportMMD.Plugin
     [PluginFilter("COM3D2x64")]
     public class ModelExportPlugin : PluginBase
     {
+        #region Constants
+
+        private static readonly string[] TPoseResetIdentityBones = new string[]
+        {
+            "Bip01",
+            "Bip01 Footsteps",
+            "Bip01 Head",
+            "Bip01 Neck",
+            "Bip01 Spine1a",
+            "Bip01 Spine1",
+            "Bip01 Spine0a",
+            "Bip01 Spine",
+            "Bip01 Pelvis",
+            "Bip01 L Thigh",
+            "Bip01 L Calf",
+            "Bip01 L Foot",
+            "Bip01 L Toe0",
+            "Bip01 L Toe01",
+            "Bip01 L Toe1",
+            "Bip01 L Toe11",
+            "Bip01 L Toe2",
+            "Bip01 L Toe21",
+            "Bip01 R Thigh",
+            "Bip01 R Calf",
+            "Bip01 R Foot",
+            "Bip01 R Toe0",
+            "Bip01 R Toe1",
+            "Bip01 R Toe11",
+            "Bip01 R Toe2",
+            "Bip01 R Toe21",
+            "Bip01 L Clavicle",
+            "Bip01 L UpperArm",
+            "Bip01 L Forearm",
+            "Bip01 L Hand",
+            "_IK_handL",
+            "Bip01 L Finger0",
+            "Bip01 L Finger01",
+            "Bip01 L Finger02",
+            "Bip01 L Finger1",
+            "Bip01 L Finger11",
+            "Bip01 L Finger12",
+            "Bip01 L Finger2",
+            "Bip01 L Finger21",
+            "Bip01 L Finger22",
+            "Bip01 L Finger3",
+            "Bip01 L Finger32",
+            "Bip01 L Finger4",
+            "Bip01 L Finger41",
+            "Bip01 L Finger42",
+            "Bip01 R Clavicle",
+            "Bip01 R UpperArm",
+            "Bip01 R Forearm",
+            "Bip01 R Hand",
+            "_IK_handR",
+            "Bip01 R Finger0",
+            "Bip01 R Finger01",
+            "Bip01 R Finger02",
+            "Bip01 R Finger1",
+            "Bip01 R Finger11",
+            "Bip01 R Finger12",
+            "Bip01 R Finger2",
+            "Bip01 R Finger21",
+            "Bip01 R Finger22",
+            "Bip01 R Finger3",
+            "Bip01 R Finger31",
+            "Bip01 R Finger32",
+            "Bip01 R Finger4",
+            "Bip01 R Finger41",
+            "Bip01 R Finger42"
+        };
+
+        private static readonly Dictionary<string, Quaternion> TPoseBoneTransformRotations = new Dictionary<string, Quaternion>
+        {
+            { "Bip01", Quaternion.Euler(-90f, 0f, 90f) },
+            { "Bip01 Head", Quaternion.Euler(0f, 0f, 15.8494f) },
+            { "Bip01 Neck", Quaternion.Euler(0f, 0f, 342.2753f) },
+            { "Bip01 Spine", Quaternion.Euler(-90f, 180f, -90f) },
+            { "Bip01 R Thigh", Quaternion.Euler(0f, 180f, 0f) },
+            { "Bip01 L Thigh", Quaternion.Euler(0f, 180f, 0f) },
+            { "Bip01 Pelvis", Quaternion.Euler(-90f, 90f, 0f) },
+            { "Bip01 R Clavicle", Quaternion.Euler(180f, -90f, 0f) },
+            { "Bip01 L Clavicle", Quaternion.Euler(180f, 90f, 0f) },
+            { "Bip01 R UpperArm", Quaternion.Euler(0f, 30f, 0f) },
+            { "Bip01 L UpperArm", Quaternion.Euler(0f, -30f, 0f) },
+            { "Bip01 R Hand", Quaternion.Euler(90f, 0f, 0f) },
+            { "Bip01 L Hand", Quaternion.Euler(-90f, 0f, 0f) },
+            { "Bip01 R Finger0", Quaternion.Euler(-70f, 55f, -30f) },
+            { "Bip01 L Finger0", Quaternion.Euler(70f, -55f, -30f) },
+            { "Bip01 L Toe0", Quaternion.Euler(10f, 0f, -80f) },
+            { "Bip01 L Toe01", Quaternion.Euler(0f, 0f, 0f) },
+            { "Bip01 L Toe1", Quaternion.Euler(-5f, 0f, -80f) },
+            { "Bip01 L Toe11", Quaternion.Euler(0f, 0f, 0f) },
+            { "Bip01 L Toe2", Quaternion.Euler(0f, 0f, -80f) },
+            { "Bip01 L Toe21", Quaternion.Euler(0f, 0f, 0f) },
+            { "Bip01 R Toe0", Quaternion.Euler(-10f, 0f, -80f) },
+            { "Bip01 R Toe01", Quaternion.Euler(0f, 0f, 0f) },
+            { "Bip01 R Toe1", Quaternion.Euler(5f, 0f, -80f) },
+            { "Bip01 R Toe11", Quaternion.Euler(0f, 0f, 0f) },
+            { "Bip01 R Toe2", Quaternion.Euler(0f, 0f, -80f) },
+            { "Bip01 R Toe21", Quaternion.Euler(0f, 0f, 0f) },
+        };
+
+        #endregion
+
         #region Fields
 
         private ModelExportWindow window;
@@ -27,7 +132,7 @@ namespace COM3D2.ModelExportMMD.Plugin
 
         public void Update()
         {
-            if (Input.GetKeyDown(KeyCode.F8))
+            if (Input.GetKeyDown(KeyCode.F8) && window != null)
             {
                 window.Show();
             }
@@ -46,14 +151,38 @@ namespace COM3D2.ModelExportMMD.Plugin
             if (window == null)
             {
                 window = new ModelExportWindow();
-                window.ExportObjCallBack = (Action)Delegate.Combine(window.ExportObjCallBack, new Action(ExportObj));
-                window.ExportPmxCallBack = (Action)Delegate.Combine(window.ExportPmxCallBack, new Action(ExportPmx));
+                window.ApplyTPoseClicked += this.ApplyTPose;
+                window.ExportObjClicked += this.ExportObj;
+                window.ExportPmxClicked += this.ExportPmx;
             }
             window.DrawWindow();
         }
 
-        private void ExportObj()
+        private void ApplyTPose(object sender, EventArgs args)
         {
+            Debug.Assert(sender == this.window);
+
+            Maid maid = GameMain.Instance.CharacterMgr.GetMaid(0);
+            maid.body0.m_Bones.GetComponent<Animation>().Stop();
+
+            Transform rootTransform = maid.body0.m_Bones.transform;
+
+            foreach (var boneName in TPoseResetIdentityBones)
+            {
+                Transform transform = CMT.SearchObjName(rootTransform, boneName);
+                transform.localRotation = Quaternion.identity;
+            }
+
+            foreach (var entry in TPoseBoneTransformRotations)
+            {
+                CMT.SearchObjName(rootTransform, entry.Key).localRotation *= entry.Value;
+            }
+        }
+
+        private void ExportObj(object sender, EventArgs args)
+        {
+            Debug.Assert(sender == this.window);
+
             try
             {
                 var filepath = GetExportFilePath(".obj");
@@ -68,8 +197,10 @@ namespace COM3D2.ModelExportMMD.Plugin
             }
         }
 
-        private void ExportPmx()
+        private void ExportPmx(object sender, EventArgs args)
         {
+            Debug.Assert(sender == this.window);
+
             try
             {
                 var filepath = GetExportFilePath(".pmx");
@@ -137,7 +268,7 @@ namespace COM3D2.ModelExportMMD.Plugin
         {
             var gameObject = FindObjectsOfType<SkinnedMeshRenderer>()
                 .Select(smr => smr.gameObject)
-                .Where(go => go.name.Contains("body"))
+                .Where(go => go.name.StartsWith("body"))
                 .FirstOrDefault();
 
             using (var w = new StreamWriter(@"BodyModel.txt"))
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@ using System.Diagnostics;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
+[assembly: AssemblyVersion("2.0.0.0")]
 [assembly: CompilationRelaxations(8)]
 [assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
 [assembly: Debuggable(DebuggableAttribute.DebuggingModes.Default | DebuggableAttribute.DebuggingModes.DisableOptimizations | DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints | DebuggableAttribute.DebuggingModes.EnableEditAndContinue)]
-[assembly: AssemblyVersion("2.0.0.0")]
